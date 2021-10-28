48cb16400b808b01f1389b30387c3a2b3ce8cdda
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-10-28 20:10:21 -0300

    add body parser

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).send("Api Ok!");
+});
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
