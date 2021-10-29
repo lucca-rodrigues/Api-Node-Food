71e35950951eb181928ed6ecef0f797de8f4e0fd
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-10-29 19:42:17 -0300

    add mongoose and connection mongoDB

--- bin/configs/variables.js
+++ bin/configs/variables.js
@@ -2,6 +2,9 @@ const variables = {
   Api: {
     port: process.env.PORT || 3000,
   },
+  Database: {
+    connection: process.env.connection || "mongodb://localhost/apifood",
+  },
 };
 
 module.exports = variables;
--- bin/express.js
+++ bin/express.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const variables = require("./configs/variables");
 
 const app = express();
@@ -7,10 +8,17 @@ const categoryRouter = require("../routes/categoryRoute");
 const productRouter = require("../routes/productRoute");
 const otherRouter = require("../routes/otherRoute");
 const classRouter = require("../routes/classRoute");
-const port = variables.Api.port;
 
+const port = variables.Api.port;
+const connection = variables.Database.connection;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connection to database
+mongoose.connect(connection, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("Welcome to the API");
