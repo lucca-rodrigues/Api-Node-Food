8ece405f6cec525233cf9700a38c2293713fa8ca
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-10-28 21:02:03 -0300

    create controller categories

---
+++ controllers/categoryController.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function categoryController() {}
+
+categoryController.prototype.get = async (req, res) => {
+  res.status(200).send("ok");
+};
+categoryController.prototype.getById = async (req, res) => {
+  res.status(200).send(`O id passado foi ${req.params.id}`);
+};
+categoryController.prototype.post = async (req, res) => {};
+categoryController.prototype.put = async (req, res) => {};
+categoryController.prototype.delete = async (req, res) => {};
+
+module.exports = categoryController;
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -2,10 +2,14 @@
 
 const express = require("express");
 const router = express.Router();
+const categoryController = require("../controllers/categoryController");
 
-router.get("/", (req, res) => {});
-router.post("/", (req, res) => {});
-router.put("/", (req, res) => {});
-router.delete("/", (req, res) => {});
+let category = new categoryController();
+
+router.get("/", category.get);
+router.get("/:id", category.getById);
+router.post("/", category.post);
+router.put("/:id", category.put);
+router.delete("/:id", category.delete);
 
 module.exports = router;
